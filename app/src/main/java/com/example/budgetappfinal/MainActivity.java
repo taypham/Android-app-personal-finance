16ceecb51680a8cdfc39f3897b5c7e43c53dfa16
taypham <xuantaypham2@gmail.com>
2020-11-12 15:09:53 -0600

    Initial commit

---
+++ app/src/main/java/com/example/budgetappfinal/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.budgetappfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnBudget;
+    private Button btnTransctions;
+    private Button btnAnalysis;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnBudget =  findViewById(R.id.btnBudget);
+        btnTransctions =  findViewById(R.id.btnTransactions);
+        btnAnalysis =  findViewById(R.id.btnAnalysis);
+
+        btnBudget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        btnTransctions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TransactionsActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        btnAnalysis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AnalysisActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
