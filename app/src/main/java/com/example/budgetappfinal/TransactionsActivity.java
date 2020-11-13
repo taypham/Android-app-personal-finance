571b1b9ef327d5e56cd8b9c7aae6e038d2f5c24e
lauraacosta <lgacosta@uark.edu>
2020-11-12 18:07:27 -0600

    Design updated with description

--- app/src/main/java/com/example/budgetappfinal/TransactionsActivity.java
+++ app/src/main/java/com/example/budgetappfinal/TransactionsActivity.java
@@ -1,11 +1,25 @@
 package com.example.budgetappfinal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class TransactionsActivity extends AppCompatActivity {
-
+    private EditText Description,amount, actualDate;
+    private Spinner spCategory;
+    private Button btnClear, btnSubmit;
+    private Date date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
