dcf6345187dfce2e39b177e93e6ca46a4bac598b
lauraacosta <lgacosta@uark.edu>
2020-11-12 18:37:05 -0600

    Design updated with description

--- app/src/main/java/com/example/budgetappfinal/TransactionsActivity.java
+++ app/src/main/java/com/example/budgetappfinal/TransactionsActivity.java
@@ -15,14 +15,90 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+/*
+import wallyson.com.br.mypocket.presenter.SpendingActivityPresenter;
+import wallyson.com.br.mypocket.presenter.SpendingInterface;*/
+
 public class TransactionsActivity extends AppCompatActivity {
-    private EditText Description,amount, actualDate;
+    private EditText description, amount, actualDate;
     private Spinner spCategory;
     private Button btnClear, btnSubmit;
     private Date date;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
+
+        description = (EditText) findViewById(R.id.editTextDescription);
+        amount = (EditText) findViewById(R.id.editTextAmount);
+        actualDate = (EditText) findViewById(R.id.editTextDate);
+        spCategory = (Spinner) findViewById(R.id.spCategory);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        date = new Date(System.currentTimeMillis());
+        SimpleDateFormat Dt = new SimpleDateFormat("DD/MM/YYYY");
+        actualDate.setText(Dt.format(date));
+        actualDate.setEnabled(false);
+
+        /*btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clean();
+            }
+        });
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ( mPresenter.spendingRegistration() )
+                    finish();
+            }
+        });
+
+        public void clear() {
+            description.requestFocus();
+            description.setText(null);
+            amount.setText(null);
+            actualDate.setText(null);
+            spCategory.setSelection(0);
+        }
+        public void CategorySpinner() {
+            final String[] Category = {
+
+
+            };
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, Category);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spCategory.setAdapter(adapter);
+
+        }
+        public String getDescription() {
+            return description.getText().toString();
+        }
+
+        public String getAmount() {
+            return amount.getText().toString();
+        }
+
+        public String getEmissionDate() {
+            return actualDate.getText().toString();
+        }
+
+        public String getCategory() {
+            return spCategory.getSelectedItem().toString();
+        }
+        public void successfullyInserted() {
+            Toast.makeText(SpendingActivity.this, getResources().getString(R.string.successfully_registration), Toast.LENGTH_SHORT).show();
+        }
+
+        public void databaseInsertError() {
+            Toast.makeText(SpendingActivity.this, getResources().getString(R.string.database_insert_error), Toast.LENGTH_SHORT).show();
+        }
+
+        public void registrationError() {
+            Toast.makeText(SpendingActivity.this, getResources().getString(R.string.registration_error), Toast.LENGTH_SHORT).show();
+        }*/
     }
 }
